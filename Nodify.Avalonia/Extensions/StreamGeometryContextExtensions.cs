5831211a0643ed79804b6c2bd754c0b30f9649e9
Johnson Zhong <zzhong@beckman.com>
2024-10-16 10:03:00 +0800

    feat: add Extensions.

---
+++ Nodify.Avalonia/Extensions/StreamGeometryContextExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Media;
+
+namespace Nodify.Avalonia.Extensions
+{
+    public static class StreamGeometryContextExtensions
+    {
+        public static void BezierTo(this StreamGeometryContext e, Point point1, Point point2, Point point3, bool isStroked, bool isSmoothJoin)
+        {
+            e.CubicBezierTo(point1, point2, point3);
+        }
+    }
+}
