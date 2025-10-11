9976544aceb6ae3f601856af79908fce06de6a74
Johnson Zhong <zzhong@beckman.com>
2025-10-11 14:18:52 +0800

    feat: add Nodify.Avalonia porject.

---
+++ Avalonia/Nodify.Avalonia/Wpf.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Windows
+{
+    namespace Controls
+    {
+    }
+
+    namespace Documents
+    {
+    }
+
+    namespace Media
+    {
+    }
+
+    namespace Input
+    {
+    }
+
+    namespace Shapes
+    {
+    }
+}
